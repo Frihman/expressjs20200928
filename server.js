7ec1365c1fd78a7a7e11b7dea6b0c84fbac417bc
James Frih <jamesfrih@me.com>
2020-09-28 11:21:39 +0200

    edit6

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ var app = express();
 
 //app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 app.get('/:id', (req, res) => {
     if (req.params.id == 'about') {
        res.sendFile(__dirname + '/about.html');
+    } else {
+        res.sendFile(__dirname + '/index.html');
     }
-    res.sendFile(__dirname + '/index.html');
+
+    
 });
 
 
